472e4ff5e428db965b33d843ff94e72c0baa0829
Tsvetan Tsvetkov <tsvetkovt@tsvetkovt-a02.vmware.com>
2018-12-28 15:58:05 +0200

    Initial skin lesure detection service commit. Some categorization functionality. No controller yet

---
+++ skin-lesion-detection-service/src/model/lesion_detection_model.py
@@ -0,0 +1,55 @@
+import os
+from keras.models import load_model
+import keras.metrics
+import keras.preprocessing.image as image
+import numpy
+
+def top_3_accuracy(y_true, y_pred):
+    return keras.metrics.top_k_categorical_accuracy(y_true, y_pred, k=3)
+
+def top_2_accuracy(y_true, y_pred):
+    return keras.metrics.top_k_categorical_accuracy(y_true, y_pred, k=2)
+
+keras.metrics.top_3_accuracy = top_3_accuracy
+keras.metrics.top_2_accuracy = top_2_accuracy
+
+class LesionDetectionModel:
+
+    script_dir = os.path.dirname(__file__)
+    root = os.path.abspath(os.path.join(os.path.join(script_dir, os.pardir), os.pardir))
+    rel_path = 'model-weigths/model.h5'
+
+    H5_MODEL_WEIGTHS_PATH = os.path.join(root, rel_path)
+
+    # Retrieved from the training session. It is the ordered list of the folders.
+    LABEL_MAP = {0: 'akiec', 1: 'bcc', 2: 'bkl', 3: 'df', 4: 'mel', 5: 'nv', 6: 'vasc'}
+
+    def __init__(self):
+        self._lesion_categorization_model = self._load_model_from_h5_file()
+
+    def _load_model_from_h5_file(self):
+
+        return load_model(self.H5_MODEL_WEIGTHS_PATH)
+
+    def predict(self, lesion_image):
+        '''
+        :param lesion_image:  A PIL Image instance.
+        :return: A probability array
+        '''
+
+        img_arr = image.img_to_array(lesion_image)
+        img_arr = numpy.expand_dims(img_arr, axis=0)
+
+        #multi dim to one dim
+        ready_image = numpy.vstack([img_arr])
+
+        return self._lesion_categorization_model.predict(ready_image)
+
+    def get_most_probable_result(self, prob):
+        '''
+        :param prob: Probability array returned from the predict method
+        :return: (Most probable label, probability)
+        '''
+        label_index = numpy.argmax(prob, axis=-1)[0]
+
+        return self.LABEL_MAP[label_index], prob[0, label_index]
---
+++ skin-lesion-detection-service/test/test_lesion_detection_model.py
@@ -0,0 +1,102 @@
+import os
+import unittest
+import src.model.lesion_detection_model as ldm
+
+from keras.preprocessing import image
+
+class LesionDetectionTest(unittest.TestCase):
+
+    RESOURCES_PATH = os.path.abspath(os.path.dirname(__file__))
+
+    def setUp(self):
+        self._model = ldm.LesionDetectionModel()
+
+    def tearDown(self):
+        self._model = None
+
+    def _load_image(self, path):
+        img = image.load_img(path, target_size=(224, 224))
+
+        return img
+
+    def test_nv_lesion(self):
+        img = self._load_image(os.path.join(self.RESOURCES_PATH, 'resources/nv.jpg'))
+
+        result = self._model.predict(img)
+
+        print(result)
+
+        label, probability = self._model.get_most_probable_result(result)
+
+        print(label)
+        print(probability)
+
+    def test_akiec_lesion(self):
+        img = self._load_image(os.path.join(self.RESOURCES_PATH, 'resources/akiec.jpg'))
+
+        result = self._model.predict(img)
+
+        print(result)
+
+        label, probability = self._model.get_most_probable_result(result)
+
+        print(label)
+        print(probability)
+
+    def test_bcc_lesion(self):
+        img = self._load_image(os.path.join(self.RESOURCES_PATH, 'resources/bcc.jpg'))
+
+        result = self._model.predict(img)
+
+        print(result)
+
+        label, probability = self._model.get_most_probable_result(result)
+
+        print(probability)
+
+    def test_bkl_lesion(self):
+        img = self._load_image(os.path.join(self.RESOURCES_PATH, 'resources/bkl.jpg'))
+
+        result = self._model.predict(img)
+
+        print(result)
+
+        label, probability = self._model.get_most_probable_result(result)
+
+        print(probability)
+
+    def test_df_lesion(self):
+        img = self._load_image(os.path.join(self.RESOURCES_PATH, 'resources/df.jpg'))
+
+        result = self._model.predict(img)
+
+        print(result)
+
+        label, probability = self._model.get_most_probable_result(result)
+
+        print(probability)
+
+    def test_mel_lesion(self):
+        img = self._load_image(os.path.join(self.RESOURCES_PATH, 'resources/mel.jpg'))
+
+        result = self._model.predict(img)
+
+        print(result)
+
+        label, probability = self._model.get_most_probable_result(result)
+
+        print(probability)
+
+    def test_vasc_lesion(self):
+        img = self._load_image(os.path.join(self.RESOURCES_PATH, 'resources/vasc.jpg'))
+
+        result = self._model.predict(img)
+
+        print(result)
+
+        label, probability = self._model.get_most_probable_result(result)
+
+        print(probability)
+
+if __name__ == '__main__':
+    unittest.main()
