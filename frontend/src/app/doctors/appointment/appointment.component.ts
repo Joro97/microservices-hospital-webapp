0aed6e142b2f4f8ba6fc18b6696a000aa9e6d8e0
otsvetkov <otsvetkov@vmware.com>
2018-12-30 12:41:56 +0200

    Introduce doctor appointment ccomponent (responsive calendar)

---
+++ frontend/src/app/doctors/appointment/appointment.component.ts
@@ -0,0 +1,56 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewChild,
+  TemplateRef
+} from '@angular/core';
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours
+} from 'date-fns';
+import { Subject } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent,
+  CalendarView
+} from 'angular-calendar';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
+
+@Component({
+  selector: 'app-appointment',
+  templateUrl: './appointment.component.html',
+  styleUrls: ['./appointment.component.css']
+})
+export class AppointmentComponent {
+  view: CalendarView = CalendarView.Month;
+
+  CalendarView = CalendarView;
+
+  viewDate: Date = new Date();
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    alert(date);
+  }
+}
