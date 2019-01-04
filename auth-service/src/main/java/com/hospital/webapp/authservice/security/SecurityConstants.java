f677c355f433ea4c5f2af3216f69c283a77933b7
Joro97 <gwave97@gmail.com>
2019-01-04 12:36:10 +0200

    Removed old auth-service

--- auth-service/src/main/java/com/hospital/webapp/authservice/security/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.webapp.authservice.security;
-
-public class SecurityConstants {
-    public static final String SECRET = "BIGSECRET";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-    public static final String[] NON_AUTH_URLS =  {"/api/register/user"};
-}
