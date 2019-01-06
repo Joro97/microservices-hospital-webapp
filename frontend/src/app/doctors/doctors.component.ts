b21caef1057e5cd04a1999003442caed4ca69c4b
Joro97 <gwave97@gmail.com>
2019-01-06 14:35:38 +0200

    Changed default DB properties to embedded Derby

--- frontend/src/app/doctors/doctors.component.ts
+++ frontend/src/app/doctors/doctors.component.ts
@@ -1,23 +1,20 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import { Subscription } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
 import { Doctor } from '../_models/doctor';
 import { DoctorService } from '../_services/doctor.service';
-import {AuthenticationService} from '../_services/authentication.service';
-declare var $: any;
+// declare var $: any;
 
 @Component({
   selector: 'app-doctors',
   templateUrl: './doctors.component.html',
   styleUrls: ['./doctors.component.css']
 })
-export class DoctorsComponent implements OnInit, OnDestroy {
+export class DoctorsComponent implements OnInit {
   doctors: Doctor[];
   filteredDoctors: Doctor[];
 
   constructor(
-    private doctorService: DoctorService,
-    private authenticationService: AuthenticationService) {
-  }
+    private doctorService: DoctorService
+  ) { }
 
   ngOnInit() {
     this.getDoctors();
@@ -53,10 +50,6 @@ export class DoctorsComponent implements OnInit, OnDestroy {
             }
         });
       });*/
-  }
-
-  ngOnDestroy(): void {
-    // this.currentUserSubscription.unsubscribe();
   }
 
   getDoctors(): void {
