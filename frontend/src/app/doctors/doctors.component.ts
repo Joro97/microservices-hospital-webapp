e40eab0819efa2a56657e577e0919b32f9f26053
Joro97 <gwave97@gmail.com>
2019-01-06 16:57:30 +0200

    Added FE pagination with the ngx-pagination library

--- frontend/src/app/doctors/doctors.component.ts
+++ frontend/src/app/doctors/doctors.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Doctor } from '../_models/doctor';
 import { DoctorService } from '../_services/doctor.service';
-// declare var $: any;
 
 @Component({
   selector: 'app-doctors',
@@ -13,43 +12,11 @@ export class DoctorsComponent implements OnInit {
   filteredDoctors: Doctor[];
 
   constructor(
-    private doctorService: DoctorService
+    private doctorService: DoctorService,
   ) { }
 
   ngOnInit() {
     this.getDoctors();
-  }
-
-  applyOnClick() {
-    /*$(document).ready(function() {
-        $('.material-card > .mc-btn-action').click(function () {
-            const card = $(this).parent('.material-card');
-            const icon = $(this).children('i');
-            icon.addClass('fa-spin-fast');
-  
-            if (card.hasClass('mc-active')) {
-                card.removeClass('mc-active');
-  
-                window.setTimeout(function() {
-                    icon
-                        .removeClass('fa-arrow-left')
-                        .removeClass('fa-spin-fast')
-                        .addClass('fa-bars');
-  
-                }, 800);
-            } else {
-                card.addClass('mc-active');
-  
-                window.setTimeout(function() {
-                    icon
-                        .removeClass('fa-bars')
-                        .removeClass('fa-spin-fast')
-                        .addClass('fa-arrow-left');
-  
-                }, 800);
-            }
-        });
-      });*/
   }
 
   getDoctors(): void {
@@ -62,7 +29,6 @@ export class DoctorsComponent implements OnInit {
   valueChange(searchText: string) {
     const filter = searchText.toLowerCase();
     this.filteredDoctors = this.doctors.filter(
-        d => d.username.toLowerCase().includes(filter) || d.specialty.toLowerCase().includes(filter)
-    );
+      d => d.username.toLowerCase().includes(filter) || d.specialty.toLowerCase().includes(filter));
   }
 }
