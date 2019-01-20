2b8830df58880542069a95d31f22118bc8f22d20
otsvetkov <otsvetkov@vmware.com>
2019-01-20 16:56:12 +0200

    Adding a footer + loading bar package to package.json.
    Footer is responsive, and cool af.

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { IconsrowComponent } from './iconsrow/iconsrow.component';
 import { VideoComponent } from './video/video.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     IconsrowComponent,
     VideoComponent,
+    FooterComponent,
   ],
   imports: [],
   exports: [
