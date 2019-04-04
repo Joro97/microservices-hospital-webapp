82a2df52daf3243cfce322c027a001cacb9688f4
Joro97 <gwave97@gmail.com>
2019-04-04 17:49:46 +0300

    Added initial version of Zuul based API gateway

---
+++ api-gateway/src/main/java/com/hospital/webapp/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.hospital.webapp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
